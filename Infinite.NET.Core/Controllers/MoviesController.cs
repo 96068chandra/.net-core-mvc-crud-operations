4fad63a26b1d57f8d3181f6cb330ddb3aefb9e7d
Chandrashekar Jogiyappa <chandrashekarj@infinite.com>
2023-01-24 15:48:05 +0530

    Add project files.

---
+++ Infinite.NET.Core/Controllers/MoviesController.cs
@@ -0,0 +1,72 @@
+﻿using Infinite.NET.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Infinite.NET.Core.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public readonly IConfiguration _configuration;
+        public MoviesController(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<MovieViewModel> movies = new();
+            using(var client=new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                var result = await client.GetAsync("Movies/GetAllMovies");
+                if (result.IsSuccessStatusCode)
+                {
+                    movies = await result.Content.ReadAsAsync<List<MovieViewModel>>();
+                }
+                return View(movies);
+            }
+        }
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            MovieViewModel movie = null;
+            using(var client=new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                var result = await client.GetAsync($"Movies/GetMovieById/{id}");
+                if(result.IsSuccessStatusCode)
+                {
+                    movie=await result.Content.ReadAsAsync<MovieViewModel>();
+                }
+
+            }
+            return View(movie);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(MovieViewModel movie)
+        {
+            if(ModelState.IsValid)
+            {
+                using(var client=new HttpClient())
+                {
+                    client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                    var result = await client.PostAsJsonAsync($"Movies/CreateMovie",movie);
+                    if (result.StatusCode == System.Net.HttpStatusCode.Created)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(movie);
+        }
+
+    }
+}
---
+++ Infinite.NET.Core/Models/MovieViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infinite.NET.Core.Models
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please provide movie name")]
+        [Display(Name ="Movie Name")]
+        public string MovieName { get; set; }
+
+        public string ProductionName { get; set; }
+        [Required(ErrorMessage ="Please provide valid release date")]
+        [Display(Name ="Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        //public GenreViewModel Genre { get; set; }
+
+        [Required]
+        [Display(Name ="Genre")]
+        public int GenreId { get; set; }
+
+        public string GenreName { get; set; }
+    }
+}
